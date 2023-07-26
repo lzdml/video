011b3c4e268704e28242d345a5e89d569ffc8651
dulin <1712288624@qq.com>
2023-07-26 12:19:31 +0800

    update vite config and github workflow

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,23 @@
  * @Author: DZL
  * @Date: 2023-07-26 10:40:54
  * @LastEditors: DZL
- * @LastEditTime: 2023-07-26 12:04:10
- * @Description: 
+ * @LastEditTime: 2023-07-26 12:18:55
+ * @Description:
  */
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/video/'
-})
+  base: "/video/",
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name]-[hash].js", // 引入文件名的名称
+        entryFileNames: "js/[name]-[hash].js", // 包的入口文件名称
+        assetFileNames: "[ext]/[name]-[hash].[ext]", // 资源文件像 字体，图片等
+      },
+    },
+  },
+});
