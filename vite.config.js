a7ebbfd8ec81195e6be6fe4decbc03d90d0c928a
dulin <1712288624@qq.com>
2023-07-26 12:04:22 +0800

    chore: update config base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: DZL
+ * @Date: 2023-07-26 10:40:54
+ * @LastEditors: DZL
+ * @LastEditTime: 2023-07-26 12:04:10
+ * @Description: 
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/video/'
 })
